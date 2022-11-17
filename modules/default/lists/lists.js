9ccb546e1d6016eada1e07f70b7892669afe88d6
Aashish Mehta <aashish_mehta@outlook.com>
2022-11-17 23:52:43 +0000

    Test implementation for lists

--- modules/default/lists/lists.js
+++ modules/default/lists/lists.js
@@ -2,12 +2,20 @@ Module.register("lists", {
     // Default module config.
     defaults: {
       text: "Hello World!",
+      list: ["Item 1", "Item 2", "Item 3"]
     },
   
     // Override dom generator.
     getDom: function () {
       var wrapper = document.createElement("div");
-      wrapper.innerHTML = this.config.text;
+
+      this.config.list.array.forEach(listItem => {
+        var itemWrapper = document.createElement("td");
+        
+        itemWrapper.innerHTML = listItem;
+        wrapper.appendChild(itemWrapper)
+      });
+
       return wrapper;
     },
   });
